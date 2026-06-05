520d27d63530e4c33c98b531415f6a0c272bf888
kusonnim <kusonnim@gmail.com>
2026-06-05 20:24:18 +0000

    a

--- week5/app.js
+++ week5/app.js
@@ -3,6 +3,8 @@ const session = require('express-session');
 
 const app = express();
 
+let sessionId = 1;
+
 app.use(session({
 	secret: 'asdf',
 	resave: false,
@@ -10,19 +12,42 @@ app.use(session({
 }));
 
 app.get('/login', (req, res) => {
+	const {user} = req.session;
+	if(user) return res.send("booooo");
+
 	const name = req.query.name;
+	const id = sessionId++;
+
+	req.session.user = {
+		id: id,
+		name: name,
+	};
+
+	return res.send(`Hi, ${name}, your sessionId is ${id}`);
 });
 
 app.get('/logout', (req, res) => {
-
+	req.session.destroy(err => {
+		if(err) return res.send("booboo");
+		return res.send("choochoo");
+	});
 });
 
 app.get('/tell', (req, res) => {
 	const msg = req.query.msg;
+	const { user } = req.session;
+
+	if(!user) return res.send("U R not logged in");
+	user.msg = msg;
+	return res.send("I remember U");
 });
 
 app.get('/ask', (req, res) => {
+	const {user} = req.session;
+	if(!user) return res.send("U R not logged in");
+	const {id, name, msg} = user;
 
+	return res.send(`${id} ${name} ${msg}`);
 });
 
 app.listen(8080, async ()=>{
